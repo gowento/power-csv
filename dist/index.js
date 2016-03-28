02af609aaabaaf07db864703f31133ba44b0bfa0
Loris Guignard <loris.guignard@gmail.com>
2016-03-28 17:21:45 +0200

    Build files

--- dist/index.js
+++ dist/index.js
@@ -6,6 +6,11 @@ Object.defineProperty(exports, "__esModule", {
 exports.csv2json = csv2json;
 exports.json2csv = json2csv;
 
+exports.default = function (req, res, next) {
+  res.csv = expressDecorator;
+  next();
+};
+
 var _babyparse = require('babyparse');
 
 var _babyparse2 = _interopRequireDefault(_babyparse);
@@ -35,4 +40,20 @@ function json2csv(json) {
   var fields = (0, _lodash2.default)(data).map(_lodash2.default.keys).flatten().uniq().value();
 
   return _babyparse2.default.unparse({ fields: fields, data: data }, opts);
+}
+
+function expressDecorator(rawData) {
+  var filename = arguments.length <= 1 || arguments[1] === undefined ? 'res.csv' : arguments[1];
+  var opts = arguments[2];
+
+  this.set('Content-Type', 'text/csv; charset=utf-8');
+  this.set('Content-Disposition', 'attachment; filename=' + filename);
+  this.status(200); // Fix for Chrome (CSV download fail if status is not 200)
+
+  var data = (0, _lodash2.default)(rawData).castArray().map(function (item) {
+    return item.toJSON ? item.toJSON() : item;
+  }).value();
+
+  var csv = json2csv(data, opts);
+  this.send(csv);
 }
